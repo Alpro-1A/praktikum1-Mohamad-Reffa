2d573a7dd412b32ee146ffd1897c88ccf75ff0e7
rffaprta <reffatama@gmail.com>
2023-10-13 20:46:58 +0700

    menambahkan file main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "-----------------------------------------------------------------------" << endl;
+    cout << "Nama : Mohamad Reffa Al Pratama" << endl << endl;
+    cout << "NPM : 2310631170031"<< endl << endl;
+    cout << "Kelas : 1A Informatika"<< endl << endl;
+    cout << "Cita-cita : Software Engineer"<< endl << endl;
+    cout << "Motivasi Masuk Informatika : Punya role model seorang software engineer"<< endl << endl;
+    cout << "-----------------------------------------------------------------------"<< endl;
+
+}
